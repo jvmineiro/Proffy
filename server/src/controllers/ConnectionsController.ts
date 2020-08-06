0e48d6ad6f0fab1011a82caaef64c701b0f25654
Joao Vitor Mineiro <joaovitor_reismineiro@hotmail.com>
2020-08-06 19:47:12 -0300

    4 dia finalizado

--- server/src/controllers/ConnectionsController.ts
+++ server/src/controllers/ConnectionsController.ts
@@ -1,22 +1,24 @@
-import {Request, Response} from 'express';
-import db from '../database/connection';
+/* eslint-disable camelcase */
+/* eslint-disable class-methods-use-this */
+import { Request, Response } from 'express';
+import db from '../db/connection';
 
 export default class ConnectionsController {
-    async index(request: Request, response: Response){
-        const totalConnections = await db('connections').count('* as total');
+  async index(req: Request, res: Response) {
+    const totalConnections = await db('connections').count('* as total');
 
-        const { total } = totalConnections[0];
+    const { total } = totalConnections[0];
 
-        return response.json({ total });
-    }
-    
-    async create(request: Request, response: Response){
-        const{ user_id } = request.body;
+    return res.json({ total });
+  }
 
-        await db('connections').insert({
-            user_id,
-        });
+  async create(req: Request, res: Response) {
+    const { user_id } = req.body;
 
-        return response.status(201).send();
-    }
+    await db('connections').insert({
+      user_id,
+    });
+
+    return res.status(201).send();
+  }
 }
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -5,12 +5,12 @@ import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
 const classesControllers = new ClassesController();
-const connectionsController = new ConnectionsController();
+const connectionsControllers = new ConnectionsController();
 
 routes.get('/classes', classesControllers.index);
 routes.post('/classes', classesControllers.create);
 
-routes.get('/connections', connectionsController.index);
-routes.post('/connections', connectionsController.create);
+routes.get('/connections', connectionsControllers.index);
+routes.post('/connections', connectionsControllers.create);
 
-export default routes; 
+export default routes;
